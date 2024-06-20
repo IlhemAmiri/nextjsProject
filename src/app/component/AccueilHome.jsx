86ff0603ec56aceba8c4ea55c0edf69e586a54b4
Ilhem <ilhem.amiri22@gmail.com>
2024-06-20 21:09:42 +0200

    best

--- src/app/component/AccueilHome.jsx
+++ src/app/component/AccueilHome.jsx
@@ -22,7 +22,7 @@ const AccueilHome = ({ isAuth, handleLogout }) => {
             <a href="#" className="hover:text-[#1ECB15] font-outfit font-semibold text-sm lg:text-base">FAQ</a>
           </nav>
         </div>
-        <div className="md:hidden flex items-center">
+        <div className="md:hidden flex items-center ml-auto">
           <button onClick={() => setMenuOpen(!menuOpen)} className="focus:outline-none">
             <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
@@ -45,7 +45,7 @@ const AccueilHome = ({ isAuth, handleLogout }) => {
         )}
       </div>
       {menuOpen && (
-        <div className="md:hidden bg-[rgba(41,41,41,0.8)] text-white flex flex-col items-center space-y-4 py-4">
+        <div className="md:hidden bg-[rgba(41,41,41,0.8)] text-white flex flex-col items-center space-y-4 py-4 items-end pr-6">
           <a href="#" className="hover:text-[#1ECB15] font-outfit font-semibold text-sm">Home</a>
           <a href="/cars" className="hover:text-[#1ECB15] font-outfit font-semibold text-sm">Cars</a>
           <a href="#" className="hover:text-[#1ECB15] font-outfit font-semibold text-sm">Booking</a>
@@ -55,20 +55,20 @@ const AccueilHome = ({ isAuth, handleLogout }) => {
           {isAuth ? (
             <button
               onClick={handleLogout}
-              className="bg-[#1ECB15] text-white items-center justify-center rounded w-28 h-9 font-extrabold text-sm tracking-wide font-outfit transition-transform hover:scale-105"
+              className="bg-[#1ECB15] text-white flex items-center justify-center rounded w-28 h-9 font-extrabold text-sm tracking-wide font-outfit transition-transform hover:scale-105"
             >
               Logout
             </button>
           ) : (
             <Link href="/signin">
-              <div className="bg-[#1ECB15] text-white items-center justify-center rounded w-28 h-9 font-extrabold text-sm tracking-wide font-outfit transition-transform hover:scale-105">
+              <div className="bg-[#1ECB15] text-white flex items-center justify-center rounded w-28 h-9 font-extrabold text-sm tracking-wide font-outfit transition-transform hover:scale-105">
                 Sign In
               </div>
             </Link>
           )}
         </div>
       )}
-        <div className="flex flex-col justify-start h-full px-6 lg:px-12 mt-16 lg:mt-32">
+      <div className="flex flex-col justify-start h-full px-6 lg:px-12 mt-16 lg:mt-32">
         <div className="w-full lg:w-[600px]">
           <h1 className="font-outfit font-medium text-white text-3xl lg:text-5xl leading-tight lg:leading-snug tracking-tight mb-4 lg:mb-8">
             Explore the world with comfortable car
